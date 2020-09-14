00565e5ad8a0ceb5dcc39e085e5fe7e61fd3c092
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-09-14 09:48:35 -0400

    remove test files

--- smurfs/src/components/Smurf.test.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-// import {render, screen} from '@testing-library/react'
-// import App from './App'
-    
-// test('can load smurf info', () =>{
-//     render(<Provider><App/></Provider>)
-//     const firstName = screen.findByText(/Brainey/i)
-// })
